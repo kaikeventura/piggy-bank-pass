6cdc5ec38bb1658758b0e462b8ecf40e65c0fa90
kaikeventura <kaike.gnr@hotmail.com>
2024-04-11 23:34:24 -0300

    Incluso angular material

---
+++ ui/piggy-bank-pass/src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {NgModule} from "@angular/core";
+
+@NgModule({
+  imports: [
+    MatSlideToggleModule,
+  ]
+})
+class AppModule {}
