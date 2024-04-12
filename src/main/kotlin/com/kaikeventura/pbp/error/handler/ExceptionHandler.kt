fe32f6ec41307a62b6daf4744d08fb9e2f15d232
kaikeventura <kaike.gnr@hotmail.com>
2024-04-11 21:24:35 -0300

    Incluso exception no handler

--- src/main/kotlin/com/kaikeventura/pbp/error/handler/ExceptionHandler.kt
+++ src/main/kotlin/com/kaikeventura/pbp/error/handler/ExceptionHandler.kt
@@ -3,6 +3,7 @@ package com.kaikeventura.pbp.error.handler
 import com.kaikeventura.pbp.error.exception.AccessNotFoundException
 import com.kaikeventura.pbp.error.exception.UserNotFoundException
 import org.springframework.http.HttpStatus
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -19,6 +20,11 @@ class ExceptionHandler {
     @ExceptionHandler(AccessNotFoundException::class)
     fun exception(ex: AccessNotFoundException) =
         ResponseError(ex.message)
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(UsernameNotFoundException::class)
+    fun exception(ex: UsernameNotFoundException) =
+        ResponseError(ex.message ?: "Unexpected error")
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception::class)
