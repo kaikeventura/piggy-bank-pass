028d1430b911a6267ba7400df34ec97a5da39cbb
kaikeventura <kaike.gnr@hotmail.com>
2024-04-12 00:15:42 -0300

    Criado estrutura para utilziação de componentes

---
+++ ui/piggy-bank-pass/src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import {Component} from '@angular/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl('', [Validators.required]);
+
+  matcher = new MyErrorStateMatcher();
+}
---
+++ ui/piggy-bank-pass/src/app/home/sign-on/sign-on.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-on',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-on.component.html',
+  styleUrl: './sign-on.component.css'
+})
+export class SignOnComponent {
+
+}
