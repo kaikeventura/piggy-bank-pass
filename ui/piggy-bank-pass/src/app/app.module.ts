eafa49ae6df5bcd23cad1605753c57ff1ebb9a72
kaikeventura <kaike.gnr@hotmail.com>
2024-04-11 23:37:36 -0300

    Removido arquivo não necessário

--- ui/piggy-bank-pass/src/app/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {NgModule} from "@angular/core";
-
-@NgModule({
-  imports: [
-    MatSlideToggleModule,
-  ]
-})
-class AppModule {}
--- ui/piggy-bank-pass/src/app/login/login.component.ts
+++ ui/piggy-bank-pass/src/app/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatSlideToggle} from "@angular/material/slide-toggle";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSlideToggle
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
